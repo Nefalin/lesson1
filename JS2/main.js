1aa55c9dd99718e73138d206a221c6af5d3c1ad2
Nefalin <nefalin1982@gmail.com>
2022-04-19 21:34:26 +0300

    JS2_v2

--- JS2/main.js
+++ JS2/main.js
@@ -1,11 +1,11 @@
 // - Є змінна х, якій ви надаєте довільне числове значення.
-let x = prompt('Input number');
+let x = +prompt('Input number');
 console.log(x);
 
 
 //     Якщо змінна a не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
-let a = prompt('Choose 1, 0 or -3');
-if (a == 0) {
+let a = +prompt('Choose 1, 0 or -3');
+if (a === 0) {
     console.log('false')
 } else {
     console.log('true')
@@ -14,7 +14,7 @@ if (a == 0) {
 
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
-let time = Number(prompt('Intup from 0 to 59'));
+let time = +prompt('Intup from 0 to 59');
 if (0 <= time && time <= 14) {
     console.log('1 part');
 } else if (14 < time && time <= 29) {
@@ -29,7 +29,7 @@ if (0 <= time && time <= 14) {
 
 
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
-let day = Number(prompt('Intup from 1 to 31'));
+let day = +prompt('Intup from 1 to 31');
 if (0 < day && day <= 10) {
     console.log('1 part');
 } else if (10 < day && day <= 20) {
@@ -42,27 +42,27 @@ if (0 < day && day <= 10) {
 
 
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
-let dayNumber = Number(prompt('Input day from 1 to 7'));
+let dayNumber = +prompt('Input day from 1 to 7');
 switch (dayNumber) {
     case 1:
         console.log('Monday');
         break;
-        case 2:
+    case 2:
         console.log('Tuesday');
         break;
-        case 3:
+    case 3:
         console.log('Wednesday');
         break;
-        case 4:
+    case 4:
         console.log('Thursday');
         break;
-        case 5:
+    case 5:
         console.log('Friday');
         break;
-        case 6:
+    case 6:
         console.log('Saturday');
         break;
-        case 7:
+    case 7:
         console.log('Sunday');
         break;
     default:
@@ -73,8 +73,8 @@ switch (dayNumber) {
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
-let first = Number(prompt('Input First number'));
-let second = Number(prompt('Input Second number'));
+let first = +prompt('Input First number');
+let second = +prompt('Input Second number');
 if (first>second) {
     console.log('First Number');
 } else if (first<second) {
